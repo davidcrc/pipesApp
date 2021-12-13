b598431534d9a8c4edb94c0ade899cf659df4cbe
davidcrc <david_drcr@hotmail.com>
2021-12-13 12:35:01 -0500

    10-22. I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,12 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class NoComunesComponent {
+  // i18nSelect
   nombre: string = 'Pedro';
   genero: string = 'femenino';
 
   invitacionMap = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
+  };
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Pepe'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    other: 'tenemos # clientes esperando.',
   };
 
   constructor() {}
